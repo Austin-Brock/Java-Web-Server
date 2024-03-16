b08949cdd6c51b07b8d9a55a71a7eeb43d413b1c
Austin-Brock <135121147+Austin-Brock@users.noreply.github.com>
2024-03-16 13:29:00 -0400

    updated file to establish a listening socket on a port

--- webServer.java
+++ webServer.java
@@ -4,7 +4,13 @@ import java.util.*;
 
 public final class WebServer {
     public static void main(String argv[]) throws Exception {
-        int port = 6789;
+        int port = 6789; //setting port number
+
+        // Establish the listen socket on the specified port
+        ServerSocket listenSocket = new ServerSocket(port);
+
+        // Print a start-up message
+        System.out.println("WebServer listening on port " + port);
     } //main
 } //webserver
 
