c43e568b6a541ab675ee588b0ed10b9fe11fbeb3
Austin-Brock <135121147+Austin-Brock@users.noreply.github.com>
2024-03-16 13:30:55 -0400

    updated webserver class

--- webServer.java
+++ webServer.java
@@ -11,6 +11,21 @@ public final class WebServer {
 
         // Print a start-up message
         System.out.println("WebServer listening on port " + port);
+
+        // Process HTTP service requests in an infinite loop.
+        while (true) {
+            // Listen for a TCP connection request.
+            Socket connectionSocket = listenSocket.accept();
+
+            // Construct an object to process the HTTP request message.
+            HttpRequest request = new HttpRequest(connectionSocket);
+
+            // Create a new thread to process the request.
+            Thread thread = new Thread(request);
+
+            // Start the thread.
+            thread.start();
+        //  The server can be stopped by pressing Ctrl+C
     } //main
 } //webserver
 
